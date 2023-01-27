2e1178f675e295860cd6d3b041962b81c50f288c
Mark Ivkovic <m.ivkovic.dev@gmail.com>
2023-01-27 08:34:52 +0000

    missed one

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Landing from "./components/Landing"
 // data to send to header for isLoggedIn status
 const user = {
   name: "Amelie Clerique",
-  isLoggedIn: false,
+  isLoggedIn: true,
 }
 
 // Layout of components for App, send props to Header for loggedIn functionality
