6b453249ff3cc6bb5fca141954f5b3933241a53a
Mark <65822259+whippet-code@users.noreply.github.com>
2023-01-29 08:01:30 +0000

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 
 // import styles
-import './App.css'
+import "./App.css";
 
 // Import components
-import Header from "./components/Header"
-import Landing from "./components/Landing"
-
-// data to send to header for isLoggedIn status
-const user = {
-  name: "Amelie Clerique",
-  isLoggedIn: true,
-}
+import Home from "./components/Home";
+import Shop from "./components/Shop";
+import User from "./components/User";
+import Legal from "./components/Legal";
 
 // Layout of components for App, send props to Header for loggedIn functionality
-function App(props) {
+function App() {
   return (
     <div className="App">
-      <Header user={ user }/>
-      <Landing />
+      <BrowserRouter>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/shop">Shop</Link>
+          <Link to="/user">Account</Link>
+          <Link to="/legal">Legal</Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/user" element={<User />} />
+          <Route path="/legal" element={<Legal />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
 
-export default App
+export default App;
