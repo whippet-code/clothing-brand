135bb99f2d35bcd4726b339b7208cf9380782fe2
Mark <65822259+whippet-code@users.noreply.github.com>
2023-01-29 19:01:22 +0000

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Home from "./components/Home";
 import Shop from "./components/Shop";
 import User from "./components/User";
 import Legal from "./components/Legal";
+import Calc from "./components/Calc";
 
 // Layout of components for App, send props to Header for loggedIn functionality
 function App() {
@@ -20,12 +21,14 @@ function App() {
           <Link to="/shop">Shop</Link>
           <Link to="/user">Account</Link>
           <Link to="/legal">Legal</Link>
+          <Link to="/calc">Calc</Link>
         </nav>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/shop" element={<Shop />} />
           <Route path="/user" element={<User />} />
           <Route path="/legal" element={<Legal />} />
+          <Route path="/calc" element={<Calc />} />
         </Routes>
       </BrowserRouter>
     </div>
