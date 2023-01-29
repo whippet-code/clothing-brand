afc02c819c064610e10109c05d22bf8bb4e9a5d0
Mark <65822259+whippet-code@users.noreply.github.com>
2023-01-29 08:52:04 +0000

    Add files via upload

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,28 +7,30 @@ import "./Styles.css";
 // import images/logos
 import logo from "../images/thread-ginger-high-resolution-logo-black-on-transparent-background.png";
 
+// ----- Old header login / user name function --------------------
 // data to send to header for isLoggedIn status
-const user = {
-  name: "Amelie Clerique",
-  isLoggedIn: false,
-};
+// const user = {
+//   name: "Amelie Clerique",
+//   isLoggedIn: false,
+// };
+// ------ Old code for user info in header from <Header /> --------
+// {
+//   // JS to alter Welcome message - isLoggedIn?
+//   user.isLoggedIn ? (
+//     <h1 className="welcome">Welcome back, {user.name}</h1>
+//   ) : (
+//     <button type="button" className="signInButton">
+//       {" "}
+//       - - Log In - -{" "}
+//     </button>
+//   )
+// }
 
 //props for Header = user {name: isLoggedIn:}
 function Header() {
   return (
     <header className="heading">
       <img src={logo} alt="Ginger & Thread logo" className="logo" />
-      {
-        // JS to alter Welcome message - isLoggedIn?
-        user.isLoggedIn ? (
-          <h1 className="welcome">Welcome back, {user.name}</h1>
-        ) : (
-          <button type="button" className="signInButton">
-            {" "}
-            - - Log In - -{" "}
-          </button>
-        )
-      }
     </header>
   );
 }
--- src/components/User.js
+++ src/components/User.js
@@ -6,11 +6,39 @@ import "./Styles.css";
 //import Components
 import Header from "./Header";
 
+// data to send to header for isLoggedIn status
+const user = {
+  name: "Amelie Clerique",
+  isLoggedIn: true,
+};
+
 function User() {
+  // button click func
+  function clicked() {
+    alert("User is logged out");
+    document.querySelector(".welcome").style.opacity = 0;
+  }
+
   return (
     <div className="User">
       <Header />
       <h1>User</h1>
+      {
+        // JS to alter Welcome message - isLoggedIn?
+        user.isLoggedIn ? (
+          <div>
+            <h1 className="welcome">Welcome back, {user.name}</h1>
+            <button type="button" className="signInButton" onClick={clicked}>
+              Sign Out
+            </button>
+          </div>
+        ) : (
+          <button type="button" className="signInButton">
+            {" "}
+            - - Log In - -{" "}
+          </button>
+        )
+      }
     </div>
   );
 }
